74597d9d2b85f4abf9effbfbd3a25fe549e027cc
jayachandra.reddy@tavant.com <jayachandra.reddy@tavant.com>
2017-06-14 19:19:12 +0530

    ui changes 718

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DealComponent } from './deal/deal.component';
+import {ModalModule} from 'ng2-modal'; 
+import { DocumentService }from './services/document.service';
 
 /* Feature Modules */
 
 @NgModule({
   imports: [
+    ReactiveFormsModule,
+    ModalModule,
+    FormsModule,
     BrowserModule,
     HttpModule,
     RouterModule.forRoot([
@@ -25,6 +31,7 @@ import { DealComponent } from './deal/deal.component';
     HomeComponent,
     DealComponent
   ],
+  providers: [ DocumentService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,8 +1,69 @@
-import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component,OnInit } from '@angular/core';
+import { DocumentService } from '../services/document.service';
+import { DocumentType } from './documentType';
+import { Headers, Http,Response,RequestOptions } from '@angular/http';
+import { Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/catch'
 @Component({
+    
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
-    public pageTitle: string = 'Welcome';
+
+    form: FormGroup;
+
+    public pageTitle: string = 'Document Types';
+    public docTypeList =[];
+    public fieldList =[];
+    public nameEnabled: boolean=false;
+    public fieldEnable: boolean=false;
+    public fieldinput: boolean=false;
+    public errorMessage: string;
+
+    
+    docTypeName = new FormControl("", Validators.required);
+   
+    constructor(fb: FormBuilder,private ds:DocumentService,private http: Http) {
+        this.form = fb.group({
+            "documentType": this.docTypeName
+        });
+    }
+ 
+    ngOnInit() {
+        this.ds.getAllDocumentTypes().subscribe(jsondata => {
+            this.docTypeList=jsondata     
+        },  error => this.errorMessage = <any>error);   
+
+        if(this.fieldList.length>0){
+        this.fieldEnable =true;  
+        }
+     }
+    docName(){
+        //str ="{"name":this.docTypeName}";
+    // this.ds.saveDocName(str);
+        this.nameEnabled =true;
+    }
+ 
+ docType(event:any){event.target.value};
+
+    onSubmit(){
+        let body = JSON.stringify(this.form.value);
+            let headers = new Headers({ 'Content-Type': 'application/json' });
+            let options = new RequestOptions({ headers: headers,method : 'POST'});
+
+    
+        this.http.post('http://localhost:8080/documentType/create', body,options).map(res=>res.json()).subscribe((res) =>this.docTypeName = res);
+        console.log(body);
+
+    this.nameEnabled =false;
+
+    this.ds.getAllDocumentTypes().subscribe(jsondata => {
+            this.docTypeList=jsondata     
+        },  error => this.errorMessage = <any>error);   
+
+    }
+     
 }
