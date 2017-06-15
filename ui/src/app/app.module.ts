72aa0f83c1d99c3d6025b4d43dccf33e5e1f203d
Bhavsar Mahesh Ganesh <bhavsar.mahesh@tavant.com>
2017-06-15 16:52:12 +0530

    ui changes for deal data entry added tree structure

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DealComponent } from './deal/deal.component';
 import { DocumentService }from './services/document.service';
+import { TreeModule } from 'angular-tree-component';
+import { FullTreeComponent } from 'app/fulltree/fulltree.component';
+import { DealService } from './deal/deal.service';
+import { FullTreeService } from './fulltree/fulltree.service';
 
 /* Feature Modules */
 
@@ -17,6 +20,9 @@ import { DocumentService }from './services/document.service';
     FormsModule,
     BrowserModule,
     HttpModule,
+    FormsModule,
+    TreeModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: 'dealSections', component: DealComponent},
@@ -27,9 +33,10 @@ import { DocumentService }from './services/document.service';
   declarations: [
     AppComponent,
     HomeComponent,
-    DealComponent
+    DealComponent,
+    FullTreeComponent
   ],
-  providers: [ DocumentService ],
+  providers: [ DocumentService,FullTreeService,DealService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
